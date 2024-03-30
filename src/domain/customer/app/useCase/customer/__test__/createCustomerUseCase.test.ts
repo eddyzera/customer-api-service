1ada48d9c63d98c2c009811ad2ecb938fee09a96
eddyzera <esilva644@gmail.com>
2024-03-30 09:26:03 -0300

    test: should be able create a new user

--- src/domain/customer/app/useCase/customer/__test__/createCustomerUseCase.test.ts
+++ src/domain/customer/app/useCase/customer/__test__/createCustomerUseCase.test.ts
@@ -1,5 +1,22 @@
-import { describe, it, beforeEach } from 'vitest'
+import { InMemoryCustomerRepository } from 'test/inMemoryDataBase/inMemoryCustomerRepository'
+import { describe, it, beforeEach, expect } from 'vitest'
+import { CreateCustomerUseCase } from '../createCustomerUseCase'
+import { makeCustomer } from 'test/factory/makeCustomer'
+
+
+let inMemoryCustomerRepository: InMemoryCustomerRepository
+let sut: CreateCustomerUseCase
 
 describe('CreateCustomerUseCase', () => {
-  it('should be able create a new user', async() => {})
+
+  beforeEach(() => {
+    inMemoryCustomerRepository = new InMemoryCustomerRepository()
+    sut = new CreateCustomerUseCase(inMemoryCustomerRepository)
+  })
+  it('should be able create a new user', async() => {
+    const customerFaker = makeCustomer()
+    const { customer } = await sut.execute(customerFaker)
+
+    expect(customer.id).toBeTruthy()
+  })
 })
--- src/domain/customer/enterprise/entities/customer.ts
+++ src/domain/customer/enterprise/entities/customer.ts
@@ -10,6 +10,35 @@ export interface CustomerProps {
 }
 
 export class Customer extends Entity<CustomerProps> {
+
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get phone() {
+    return this.props.phone
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  set name(value) {
+    this.props.name = value
+  }
+
+  set email(value) {
+    this.props.email = value
+  }
+
+  set phone(value) {
+    this.props.phone = value
+  }
+
   static create(props: Optional<CustomerProps, 'createdAt'>,
   id?: UniqueEntityId,) {
     const customer = new Customer({
