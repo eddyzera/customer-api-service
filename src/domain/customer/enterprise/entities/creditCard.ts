e3008b023c8fe3d3f8bb27b442fe78f8bccdfc12
eddyzera <esilva644@gmail.com>
2024-03-30 09:46:23 -0300

    feat: creating creditCard entity

---
+++ src/domain/customer/enterprise/entities/creditCard.ts
@@ -0,0 +1,65 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/uniqueEntityId'
+import { Optional } from '@/core/types/optional'
+
+export interface CreditCardProps {
+  customerId: UniqueEntityId
+  cardNumber: string
+  expirationDate: number
+  securityCode: string
+  nameOnCard: number
+  createdAt: Date
+}
+
+export class CreditCard extends Entity<CreditCardProps> {
+
+  get customerId() {
+    return this.props.customerId
+  }
+
+  get cardNumber() {
+    return this.props.cardNumber
+  }
+
+  get expirationDate() {
+    return this.props.expirationDate
+  }
+
+  get securityCode() {
+    return this.props.securityCode
+  }
+
+  get nameOnCard() {
+    return this.props.nameOnCard
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  set cardNumber(value) {
+    this.props.cardNumber = value
+  }
+
+  set expirationDate(value) {
+    this.props.expirationDate = value
+  }
+
+  set securityCode(value) {
+    this.props.securityCode = value
+  }
+
+  set nameOnCard(value) {
+    this.props.nameOnCard = value
+  }
+
+  static create(props: Optional<CreditCardProps, 'createdAt'>,
+  id?: UniqueEntityId,) {
+    const creditCard = new CreditCard({
+      ...props,
+      createdAt: new Date()
+    }, id)
+
+    return creditCard
+  }
+}
