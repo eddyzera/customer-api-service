5aad465c21d32607167e9dff7b750132f89eb93f
eddyzera <esilva644@gmail.com>
2024-04-05 20:43:00 -0300

    feat: create user entity

--- src/domain/user/enterprise/entities/user.ts
+++ src/domain/user/enterprise/entities/user.ts
@@ -1,3 +1,47 @@
 import { Entity } from '@/core/entities/entity'
+import { UniqueEntityId } from '@/core/entities/uniqueEntityId'
+import { Optional } from '@/core/types/optional'
 
-export class User extends Entity {}
+export interface UserProps {
+  name: string
+  email: string
+  password: string
+  createdAt: Date
+}
+
+export class User extends Entity<UserProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  set name(value) {
+    this.props.name = value
+  }
+
+  set email(value) {
+    this.props.email = value
+  }
+
+  static create(
+    props: Optional<UserProps, 'createdAt'>,
+    id?: UniqueEntityId,
+  ) {
+    const user = new User(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    return user
+  }
+}
